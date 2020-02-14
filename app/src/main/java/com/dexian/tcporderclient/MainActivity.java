23455fbef37ae15b2211390cc4a5f5df294a195f
De Xian <ahx.agent007@gmail.com>
2020-02-14 15:55:36 +0600

    UI Customer App (Done)

--- app/src/main/java/com/dexian/tcporderclient/MainActivity.java
+++ app/src/main/java/com/dexian/tcporderclient/MainActivity.java
@@ -3,8 +3,16 @@ package com.dexian.tcporderclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btn_settings;
+    EditText ET_tableNo;
+    ListView LV_itemAvailable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
